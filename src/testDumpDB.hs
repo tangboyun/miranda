41fdd9af2cfcda9306620c3d212237f6c70985ef
Boyun Tang <tangboyun@hotmail.com>
2013-03-29 17:19:43 +0800

    test for serialization

--- src/testDumpDB.hs
+++ src/testDumpDB.hs
@@ -17,11 +17,21 @@ module Main where
 import System.Environment
 
 import MiRanda.Storage.IO
+import MiRanda.IO (mkUTRFile)
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Lazy as L
+import qualified Codec.Compression.GZip as GZip
+import Data.Monoid
 
-
-utrFile = "/tmp/testUTR.txt.tar.gz"
-miRBase = "/tmp/miRNA.dat.gz"
-outF = "/tmp/Human_lncRNA_V2.data"
-main = dumpDB "Human" utrFile miRBase outF
+geneUTR = "/home/tangboyun/miRNAPrediction/UTR_Sequences.txt"
+outUTR = "/tmp/tmpUTR.dat.gz"
+miRBase = "/home/tangboyun/miRNADB/miRNA.dat.gz"
+utrFile = "/tmp/Mouse_lncRNA_V2.dat.gz"
+dbFile = "/home/tangboyun/miRNADB/Mouse_lncRNA_V2_miRBase19.data"
+main = do 
+   (spe:fastaFile:annoFile:_) <- getArgs
+   mkUTRFile spe fastaFile annoFile outUTR
+   str <- L8.readFile geneUTR 
+   L8.readFile outUTR >>= L.writeFile utrFile . GZip.compress . L8.append str . L8.unlines . tail . L8.lines . L8.filter (/= '\r')
+   dumpDB spe utrFile miRBase dbFile
